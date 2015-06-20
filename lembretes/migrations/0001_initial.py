5fda540089bc0fb5211972f7ff12777d8f11b767
GuiLe Garcia <guilesgarcia@gmail.com>
2015-06-19 23:36:45 -0300

    teste

---
+++ lembretes/migrations/0001_initial.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Lembrete',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('titulo', models.CharField(max_length=200)),
+                ('descricao', models.TextField(null=True, blank=True)),
+                ('data', models.DateField()),
+                ('usuario', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
